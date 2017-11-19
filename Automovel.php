c2a15b2b6dba09f7e0048904e5b4c37681776852
Neto Sales <contato@netosales.com.br>
2017-11-19 18:07:00 -0300

    Arquivos da aula sobre autoload. Com erro.

---
+++ Automovel.php
@@ -0,0 +1,23 @@
+<?php
+interface Veiculo{
+	public function acelerar($velocidade);
+	public function frenar($velocidade);
+	public function trocarMarcha($marcha);
+}
+
+
+abstract class Automovel implements Veiculo{
+	public function acelerar($velocidade){
+		echo "O veiculo acelerou ateh  ". $velocidade . " Km/h";
+	}
+	public function frenar($velocidade)
+	{
+		echo "o veiculo frenou ateh " . $velocidade . " Km/h";
+	}
+	public function trocarMarcha($marcha)
+	{
+		echo "O veiculo engatou a marcha " . $marcha . " Km/h";
+	}
+}
+
+?>
